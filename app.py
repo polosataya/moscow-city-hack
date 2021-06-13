7e6e9d6b4847720878d8132366cace0d5baed186
polosataya <34760744+polosataya@users.noreply.github.com>
2021-06-14 01:10:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,13 @@ default_col = weekdays + work_time + ['crime_rank', 'money_rank', 'device_count_
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.after_request
+def after_request(response):
+   response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+   response.headers["Expires"] = 0
+   response.headers["Pragma"] = "no-cache"
+   return response
+
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 root = root_dir() + '/'
